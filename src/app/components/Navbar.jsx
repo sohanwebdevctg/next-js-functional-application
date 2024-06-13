852d805130b9968183e93a4aba07cdea0a04711d
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-06-13 16:33:59 +0600

    install react icons in next-js

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Logo from '../../../public/logo/logo.png'
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
@@ -10,6 +11,20 @@ const Navbar = () => {
           {/* logo section start */}
             <Image src={Logo} alt='logo' className='w-[10%] h-12'></Image>
           {/* logo section end */}
+          {/* link section start */}
+          <ul className='flex items-center justify-center gap-5'>
+            <li className='xl:text-sm'><Link href="/">HOME</Link></li>
+            <li className='xl:text-sm'><Link href="/">SHOP</Link></li>
+            <li className='xl:text-sm'><Link href="/">ABOUT US</Link></li>
+            <li className='xl:text-sm'><Link href="/">CONTACT US</Link></li>
+            <li className='xl:text-sm'><Link href="/">BLOG</Link></li>
+          </ul>
+          {/* link section end */}
+          {/* icon section start */}
+          <ul className='flex items-center justify-center gap-5'>
+            <li className='xl:text-sm'><Link href="/"></Link></li>
+          </ul>
+          {/* icon section end */}
         </div>
       </div>
       {/* content section end */}
