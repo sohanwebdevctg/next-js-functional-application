1ac9d7461d93f5855e88a5054e3679d3b840be73
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-06-13 20:04:18 +0600

    navbar desktop md,lg,xl mobile responsive

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -13,25 +13,25 @@ const Navbar = () => {
   return (
     <div>
       {/* content section start */}
-      <div className="container mx-auto h-24">
-        <div className="flex justify-between items-center h-full">
+      <div className="container mx-auto h-24 md:px-5 lg:px-10 xl:px-12">
+        <div className="md:flex md:justify-between md:items-center h-full">
           {/* logo section start */}
-            <Image src={Logo} alt='logo' className='w-[10%] h-10'></Image>
+            <Image src={Logo} alt='logo' className='md:w-[8%] lg:w-[9%] xl:w-[10%] md:h-5 lg:h-8 xl:h-10'></Image>
           {/* logo section end */}
           {/* link section start */}
           <ul className='flex items-center justify-center gap-8'>
-            <li className='xl:text-sm'><Link className={`link ${pathname === '/' ? 'xl:text-sm font-bold' : 'xl:text-sm'}`} href="/">HOME</Link></li>
-            <li className='xl:text-sm'><Link className={`link ${pathname === '/shop' ? 'xl:text-sm font-bold' : 'xl:text-sm'}`} href="/shop">SHOP</Link></li>
-            <li className='xl:text-sm'><Link className={`link ${pathname === '/about' ? 'xl:text-sm font-bold' : 'xl:text-sm'}`} href="/about">ABOUT</Link></li>
-            <li className='xl:text-sm'><Link className={`link ${pathname === '/contact' ? 'xl:text-sm font-bold' : 'xl:text-sm'}`} href="/contact">CONTACT</Link></li>
-            <li className='xl:text-sm'><Link className={`link ${pathname === '/blog' ? 'xl:text-sm font-bold' : 'xl:text-sm'}`} href="/blog">BLOG</Link></li>
+            <li><Link className={`link ${pathname === '/' ? 'md:text-xs lg:text-[13px] xl:text-sm font-bold' : 'md:text-xs lg:text-[13px] xl:text-sm'}`} href="/">HOME</Link></li>
+            <li><Link className={`link ${pathname === '/shop' ? 'md:text-xs lg:text-[13px] xl:text-sm font-bold' : 'md:text-xs lg:text-[13px] xl:text-sm'}`} href="/shop">SHOP</Link></li>
+            <li><Link className={`link ${pathname === '/about' ? 'md:text-xs lg:text-[13px] xl:text-sm font-bold' : 'md:text-xs lg:text-[13px] xl:text-sm'}`} href="/about">ABOUT</Link></li>
+            <li><Link className={`link ${pathname === '/contact' ? 'md:text-xs lg:text-[13px] xl:text-sm font-bold' : 'md:text-xs lg:text-[13px] xl:text-sm'}`} href="/contact">CONTACT</Link></li>
+            <li><Link className={`link ${pathname === '/blog' ? 'md:text-xs lg:text-[13px] xl:text-sm font-bold' : 'md:text-xs lg:text-[13px] xl:text-sm'}`} href="/blog">BLOG</Link></li>
           </ul>
           {/* link section end */}
           {/* icon section start */}
           <ul className='flex items-center justify-center gap-5'>
-            <li className='xl:text-[16px]'><Link href="/"><FaShoppingCart></FaShoppingCart></Link></li>
-            <li className='xl:text-[16px]'><Link href="/"><FaHeart></FaHeart></Link></li>
-            <li className='xl:text-[16px]'><Link href="/"><FaSearch></FaSearch></Link></li>
+            <li className='md:text-xs lg:text-[14px] xl:text-[16px]'><Link href="/"><FaShoppingCart></FaShoppingCart></Link></li>
+            <li className='md:text-xs lg:text-[14px] xl:text-[16px]'><Link href="/"><FaHeart></FaHeart></Link></li>
+            <li className='md:text-xs lg:text-[14px] xl:text-[16px]'><Link href="/"><FaSearch></FaSearch></Link></li>
           </ul>
           {/* icon section end */}
         </div>
