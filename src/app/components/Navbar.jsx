bf4ce33ec08d0fa1cbdf3b8fffd5bfdb02525e2e
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-06-13 16:03:23 +0600

    creating next-js function porject setup

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,18 @@
+
+
+const Navbar = () => {
+  return (
+    <div>
+      {/* content section start */}
+      <div className="container mx-auto h-20">
+        <div className="flex justify-between items-center h-full">
+          {/* logo section start */}
+          {/* logo section end */}
+        </div>
+      </div>
+      {/* content section end */}
+    </div>
+  );
+};
+
+export default Navbar;
