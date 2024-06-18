745ec090673ae6266400007b289d0955788aede9
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-06-18 17:47:44 +0600

    responsive footer section

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -13,10 +13,10 @@ const Footer = () => {
           <div>
             <h1 className='text-sm sm:text-xs md:text-sm lg:text-sm xl:text-lg uppercase font-bold mb-2 sm:mb-1 md:mb-2 lg:mb-2 xl:mb-2'>About Us</h1>
             <ul className='space-y-1 sm:space-y-0 md:space-y-1 lg:space-y-1 xl:space-y-2'>
-              <li><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>Our History</Link></li>
-              <li><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>FAQ by Support</Link></li>
-              <li><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>News & Update</Link></li>
-              <li><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>Performance</Link></li>
+              <li className='hover:text-red-500 duration-300 text-gray-500'><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>Our History</Link></li>
+              <li className='hover:text-red-500 duration-300 text-gray-500'><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>FAQ by Support</Link></li>
+              <li className='hover:text-red-500 duration-300 text-gray-500'><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>News & Update</Link></li>
+              <li className='hover:text-red-500 duration-300 text-gray-500'><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>Performance</Link></li>
             </ul>
           </div>
           {/* item1 section end */}
@@ -24,10 +24,10 @@ const Footer = () => {
           <div>
             <h1 className='text-sm sm:text-xs md:text-sm lg:text-sm xl:text-lg uppercase font-bold mb-2 sm:mb-1 md:mb-2 lg:mb-2 xl:mb-2'>Contact Us</h1>
             <ul className='space-y-1 sm:space-y-0 md:space-y-1 lg:space-y-1 xl:space-y-2'>
-              <li><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>Blog Site</Link></li>
-              <li><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>Support Line</Link></li>
-              <li><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>Social Media</Link></li>
-              <li><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>Our Website</Link></li>
+              <li className='hover:text-red-500 duration-300 text-gray-500'><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>Blog Site</Link></li>
+              <li className='hover:text-red-500 duration-300 text-gray-500'><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>Support Line</Link></li>
+              <li className='hover:text-red-500 duration-300 text-gray-500'><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>Social Media</Link></li>
+              <li className='hover:text-red-500 duration-300 text-gray-500'><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>Our Website</Link></li>
             </ul>
           </div>
           {/* item2 section end */}
@@ -35,10 +35,10 @@ const Footer = () => {
           <div>
             <h1 className='text-sm sm:text-xs md:text-sm lg:text-sm xl:text-lg uppercase font-bold mb-2 sm:mb-1 md:mb-2 lg:mb-2 xl:mb-2'>Help</h1>
             <ul className='space-y-1 sm:space-y-0 md:space-y-1 lg:space-y-1 xl:space-y-2'>
-              <li><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>About Us</Link></li>
-              <li><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>Careers</Link></li>
-              <li><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>Newsroom</Link></li>
-              <li><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>Contact Us</Link></li>
+              <li className='hover:text-red-500 duration-300 text-gray-500'><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>About Us</Link></li>
+              <li className='hover:text-red-500 duration-300 text-gray-500'><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>Careers</Link></li>
+              <li className='hover:text-red-500 duration-300 text-gray-500'><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>Newsroom</Link></li>
+              <li className='hover:text-red-500 duration-300 text-gray-500'><Link href="/" className='text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-[15px]'>Contact Us</Link></li>
             </ul>
           </div>
           {/* item3 section end */}
