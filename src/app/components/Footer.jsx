ba17f4114331dc0706d8701c03421b3f72d8703c
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-06-18 17:40:54 +0600

    responsive footer bottom card

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       {/* content section start */}
       <div className="container mx-auto">
         {/* upper section start */}
-        <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 gap-3 sm:gap-2 md:gap-4 xl:gap-10 h-full w-full py-10 md:py-10 lg:py-10 xl:py-10 px-5 sm:px-5 md:px-5 lg:px-10 xl:px-12">
+        <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 gap-3 sm:gap-1 md:gap-4 xl:gap-10 h-full w-full py-10 md:py-10 lg:py-10 xl:py-10 px-4 sm:px-5 md:px-5 lg:px-10 xl:px-12">
           {/* item1 section start */}
           <div>
             <h1 className='text-sm sm:text-xs md:text-sm lg:text-sm xl:text-lg uppercase font-bold mb-2 sm:mb-1 md:mb-2 lg:mb-2 xl:mb-2'>About Us</h1>
@@ -43,14 +43,14 @@ const Footer = () => {
           </div>
           {/* item3 section end */}
           {/* item4 section start */}
-          <div className='bg-black bg-opacity-55 shadow-xl rounded-tl-[60px] rounded-br-[60px] pr-5 flex flex-col justify-center gap-3'>
+          <div className='bg-black bg-opacity-55 shadow-xl rounded-tl-[60px] rounded-br-[60px] pr-3 sm:pr-5 md:pr-5 lg:pr-5 xl:pr-5 flex flex-col justify-center gap-0 xl:gap-2'>
             <ul className="text-right">
-              <li><Link href="/" className="text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-sm text-gray-300">+8801632937291</Link></li>
-              <li><Link href="/" className="text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-sm text-red-500">xcodeemail@gmail.com</Link></li>
+              <li><Link href="/" className="text-[9px] sm:text-[9px] md:text-[11px] lg:text-xs xl:text-xs text-gray-300">+8801632937291</Link></li>
+              <li><Link href="/" className="text-[9px] sm:text-[9px] md:text-[11px] lg:text-xs xl:text-xs text-red-500">xcodeemail@gmail.com</Link></li>
             </ul>
             <ul className="text-right">
-              <li><Link href="/" className="text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-sm text-gray-300">900-140 10th Avenue SE</Link></li>
-              <li><Link href="/" className="text-xs sm:text-[11px] md:text-[13px] lg:text-sm xl:text-sm text-gray-300">Calgary, AB T2G OR1</Link></li>
+              <li><Link href="/" className="text-[9px] sm:text-[9px] md:text-[11px] lg:text-xs xl:text-xs text-gray-300">900-140 10th Avenue SE</Link></li>
+              <li><Link href="/" className="text-[9px] sm:text-[9px] md:text-[11px] lg:text-xs xl:text-xs text-gray-300">Calgary, AB T2G OR1</Link></li>
             </ul>
           </div>
           {/* item4 section end */}
