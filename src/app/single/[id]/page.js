4cba56263fb10bd88cb535915373cbf8e2f36063
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-06-22 13:19:56 +0600

    creating dynamic single page data and show some data

--- src/app/single/[id]/page.js
+++ src/app/single/[id]/page.js
@@ -120,8 +120,25 @@ const SingleProductPage = ({params}) => {
 
 
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className='px-5 sm:px-4 md:px-4 lg:px-0'>
+      {/* content section start */}
+      <div className='container mx-auto  h-full w-full py-10 px-3 sm:px-5 md:px-4 lg:px-10 xl:px-12'>
+        {/* item section start */}
+        <div className="sm:flex sm:justify-between sm:items-center">
+          {/* image section start */}
+          <div className="w-full sm:w-[50%]">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui officia commodi magnam animi, debitis sapiente maxime iusto provident excepturi dolores impedit hic at quasi repellendus temporibus consequatur perspiciatis illo minima accusantium id sint dolore. Ea molestias aut deleniti a nulla saepe animi. Excepturi quam quibusdam atque saepe molestias illo quae unde ipsum assumenda fugit? Suscipit, cupiditate impedit ducimus quidem dolor aspernatur repudiandae voluptates assumenda sit. Dicta laboriosam ad a autem architecto voluptates nemo voluptatem, incidunt ratione quod quo cumque atque deleniti aliquam culpa, impedit minus magni perferendis officiis molestiae voluptatum. At cumque repellat tempora accusantium nihil illum incidunt, expedita dolore sapiente nesciunt exercitationem nulla rem neque pariatur obcaecati molestiae iste harum tempore provident quis! Earum inventore officia, accusamus, distinctio enim aut itaque quasi perferendis, necessitatibus non provident sint laudantium porro ea numquam maxime dolor quae? Laborum commodi a ad eum esse neque dolore iure expedita minima iusto porro aut quasi, numquam eligendi quis corrupti. Incidunt vitae atque ipsam natus exercitationem veritatis adipisci dolorem tempora unde ex hic suscipit itaque, eos ad aliquam, doloremque accusamus laborum asperiores! Deleniti dolores, beatae ullam nesciunt aut recusandae cumque soluta magni fuga ducimus, quam quidem neque sunt vitae sed quos quisquam temporibus corporis voluptates praesentium.</p>
+          </div>
+          {/* image section end */}
+          {/* details section start */}
+          <div className="w-full sm:w-[50%] bg-[#F5F5F5]">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui officia commodi magnam animi, debitis sapiente maxime iusto provident excepturi dolores impedit hic at quasi repellendus temporibus consequatur perspiciatis illo minima accusantium id sint dolore. Ea molestias aut deleniti a nulla saepe animi. Excepturi quam quibusdam atque saepe molestias illo quae unde ipsum assumenda fugit? Suscipit, cupiditate impedit ducimus quidem dolor aspernatur repudiandae voluptates assumenda sit. Dicta laboriosam ad a autem architecto voluptates nemo voluptatem, incidunt ratione quod quo cumque atque deleniti aliquam culpa, impedit minus magni perferendis officiis molestiae voluptatum. At cumque repellat tempora accusantium nihil illum incidunt, expedita dolore sapiente nesciunt exercitationem nulla rem neque pariatur obcaecati molestiae iste harum tempore provident quis! Earum inventore officia, accusamus, distinctio enim aut itaque quasi perferendis, necessitatibus non provident sint laudantium porro ea numquam maxime dolor quae? Laborum commodi a ad eum esse neque dolore iure expedita minima iusto porro aut quasi, numquam eligendi quis corrupti. Incidunt vitae atque ipsam natus exercitationem veritatis adipisci dolorem tempora unde ex hic suscipit itaque, eos ad aliquam, doloremque accusamus laborum asperiores! Deleniti dolores, beatae ullam nesciunt aut recusandae cumque soluta magni fuga ducimus, quam quidem neque sunt vitae sed quos quisquam temporibus corporis voluptates praesentium.</p>
+          </div>
+          {/* details section end */}
+        </div>
+        {/* item section end */}
+      </div>
+      {/* content section end */}
     </div>
   );
 };
