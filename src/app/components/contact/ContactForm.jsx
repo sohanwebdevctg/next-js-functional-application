0a826698b2899a69627bc4e394378140e66b6541
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-06-24 17:02:09 +0600

    creating ContactForm section and adding left side

---
+++ src/app/components/contact/ContactForm.jsx
@@ -0,0 +1,35 @@
+
+
+const ContactForm = () => {
+  return (
+    <div className="px-0 sm:px-4 md:px-4 my-16">
+      {/* content section start */}
+      <div className='container mx-auto bg-[#F5F5F5] h-full w-full p-10'>
+        <div className="sm:flex sm:justify-around">
+          {/* left item section start */}
+          <div className="flex-1 flex flex-col justify-between">
+            <h3 className="xl:text-5xl">Let's talk <br></br>on something <span className="text-red-600">great</span> <br></br>together</h3>
+            <ul className="space-y-8">
+              <li>+234 8993 98043</li>
+              <li>andreal@gmail.com</li>
+              <li>855-555-5544</li>
+              <li>123 Ctg, Bangladesh</li>
+            </ul>
+            <ul>
+              <li>andreal@gmail.com</li>
+            </ul>
+          </div>
+          {/* left item section end */}
+          {/* right item section start */}
+          <div className="flex-1">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum consequuntur repellendus quae fugit sed, expedita ratione delectus. Blanditiis obcaecati odio labore praesentium explicabo qui nam debitis dolorem sapiente assumenda, dolor harum mollitia ex, animi nemo ullam ab aliquam! Necessitatibus cupiditate est magnam. Quod nisi earum minus, provident nam consectetur alias asperiores fugit praesentium nulla omnis natus voluptas labore sit architecto fugiat optio amet, libero iure dolore. Corrupti sunt dolorum eaque omnis perferendis doloribus eligendi aperiam nam? Laborum ipsum quis optio, voluptatem in ipsa eaque labore repellendus, iusto quae animi quibusdam! Aliquam, sint esse odio ad cupiditate voluptatem. Iste repellendus harum reprehenderit similique dolor doloribus ex, dolorem facere consequatur, perferendis dolorum ducimus beatae esse voluptatum pariatur soluta explicabo laborum delectus quod provident unde quam laudantium. Vero sunt voluptatum sequi! Explicabo temporibus beatae quod porro recusandae et officiis voluptates similique officia, fuga minus est vitae ut ipsam eveniet odio! Aut commodi consequuntur rem atque officia? Quisquam vel nulla asperiores corporis nam possimus est ea dolore quis quasi, nemo rerum qui. Inventore cupiditate nihil, ad suscipit fugiat magnam quasi similique quaerat incidunt expedita eius molestiae dolorem, qui earum repellat quibusdam illo. Totam hic suscipit maiores, sunt numquam eaque mollitia dolorum. Modi, explicabo placeat?</p>l
+          </div>
+          {/* right item section end */}
+        </div>
+      </div>
+      {/* content section end */}
+    </div>
+  );
+};
+
+export default ContactForm;
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,6 +1,6 @@
-import React from 'react';
 import ContactBanner from '../components/contact/ContactBanner';
 import MapItem from '../components/contact/MapItem';
+import ContactForm from '../components/contact/ContactForm';
 
 const ContactPage = () => {
   return (
@@ -8,6 +8,9 @@ const ContactPage = () => {
       {/* contact banner section start */}
       <ContactBanner></ContactBanner>
       {/* contact banner section end */}
+      {/* contact form section start */}
+      <ContactForm></ContactForm>
+      {/* contact form section end */}
       {/* mapItem section start */}
       <MapItem></MapItem>
       {/* mapItem section end */}
