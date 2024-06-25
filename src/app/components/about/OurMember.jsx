f609cd3205ca3f215e18fc561ff92c50a593f3da
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-06-25 16:22:08 +0600

    adding ourMemeber card css raw animation and mobile,sm,md responsive

--- src/app/components/about/OurMember.jsx
+++ src/app/components/about/OurMember.jsx
@@ -4,6 +4,7 @@ import person2 from '../../../../public/about/person2.png'
 import person3 from '../../../../public/about/person3.png'
 import person4 from '../../../../public/about/person4.png'
 import person5 from '../../../../public/about/person5.png'
+import { FaFacebookSquare, FaInstagram, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
 
 
 const OurMember = () => {
@@ -19,40 +20,18 @@ const OurMember = () => {
         </div>
         {/* title section end */}
         {/* item section start */}
-        <div className="grid grid-cols-5 gap-5 my-10">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 xl:grid-cols-5 my-10">
           {/* item1 start */}
-          <div className="text-center mx-auto space-y-2">
-            <Image src={person1} alt="person 1" className="w-48 h-48 rounded-full shadow-lg mx-auto bg-slate-300 p-1"></Image>
-            <h2 className="font-bold text-lg">Abdullah(CEO)</h2>
-            <p className="text-sm italic text-gray-500">abdullah@gmail.com</p>
-          </div>
-          {/* item1 end */}
-          {/* item1 start */}
-          <div className="text-center mx-auto space-y-2">
-            <Image src={person2} alt="person 1" className="w-48 h-48 rounded-full shadow-lg mx-auto bg-slate-300 p-1"></Image>
-            <h2 className="font-bold text-lg">Abdullah(COO)</h2>
-            <p className="text-sm italic text-gray-500">abdullah@gmail.com</p>
-          </div>
-          {/* item1 end */}
-          {/* item1 start */}
-          <div className="text-center mx-auto space-y-2">
-            <Image src={person3} alt="person 1" className="w-48 h-48 rounded-full shadow-lg mx-auto bg-slate-300 p-1"></Image>
-            <h2 className="font-bold text-lg">Abdullah(HR)</h2>
-            <p className="text-sm italic text-gray-500">abdullah@gmail.com</p>
-          </div>
-          {/* item1 end */}
-          {/* item1 start */}
-          <div className="text-center mx-auto space-y-2">
-            <Image src={person4} alt="person 1" className="w-48 h-48 rounded-full shadow-lg mx-auto bg-slate-300 p-1"></Image>
-            <h2 className="font-bold text-lg">Abdullah(MD)</h2>
-            <p className="text-sm italic text-gray-500">abdullah@gmail.com</p>
-          </div>
-          {/* item1 end */}
-          {/* item1 start */}
-          <div className="text-center mx-auto space-y-2">
-            <Image src={person5} alt="person 1" className="w-48 h-48 rounded-full shadow-lg mx-auto bg-slate-300 p-1"></Image>
-            <h2 className="font-bold text-lg">Abdullah(CR)</h2>
-            <p className="text-sm italic text-gray-500">abdullah@gmail.com</p>
+          <div className="memberCard text-center mx-auto space-y-2 w-full h-52 sm:h-48 md:h-52 xl:h-[260px] p-5 overflow-hidden">
+            <Image src={person1} alt="person 1" className="image w-24 h-24 sm:w-20 sm:h-20 md:w-24 md:h-24 xl:w-32 xl:h-32 shadow-lg mx-auto"></Image>
+            <h2 className="font-bold text-sm sm:text-xs md:text-[13px] xl:text-base">Abdullah(CEO)</h2>
+            <p className="text-[11px] sm:text-[10px] md:text-[13px] xl:text-sm italic text-red-600">abdullah@gmail.com</p>
+            <ul className="social flex items-center justify-center gap-1 md:gap-2 xl:gap-3">
+                <li><FaFacebookSquare className="text-[21px] sm:text-xl md:text-[22px] lg:text-2xl xl:text-xl text-red-600"></FaFacebookSquare></li>
+                <li><FaInstagram className="text-[21px] sm:text-xl md:text-[22px] lg:text-2xl xl:text-xl text-red-600"></FaInstagram></li>
+                <li><FaLinkedin className="text-[21px] sm:text-xl md:text-[22px] lg:text-2xl xl:text-xl text-red-600"></FaLinkedin></li>
+                <li><FaTwitterSquare className="text-[21px] sm:text-xl md:text-[22px] lg:text-2xl xl:text-xl text-red-600"></FaTwitterSquare></li>
+              </ul>
           </div>
           {/* item1 end */}
 
