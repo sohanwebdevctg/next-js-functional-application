94d4fdac85969582bcd07c694492c34017701ac6
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-06-25 19:55:28 +0600

    responsive the services section

--- src/app/components/about/Services.jsx
+++ src/app/components/about/Services.jsx
@@ -3,29 +3,96 @@ import { MdAddShoppingCart } from 'react-icons/md';
 
 const Services = () => {
   return (
-    <div className="py-5 sm:py-5 md:py-5 lg:py-8 xl:py-12 bg-black bg-opacity-40">
+    <div className="py-5 sm:py-5 md:py-5 lg:py-8 xl:py-12 bg-black bg-opacity-35">
       {/* content section start */}
       <div className='container h-full w-full mx-auto px-5 md:px-5 lg:px-10 xl:px-12'>
         {/* item section start */}
-        <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-2 sm:gap-5 md:gap-3 lg:gap-5 xl:gap-5'>
+        <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5'>
           {/* item1 section start */}
           <ul>
             <li>
               {/* icon section start */}
               <div>
-                <MdAddShoppingCart className="text-center mx-auto text-3xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-5xl text-red-600"></MdAddShoppingCart>
+                <h3 className="text-center mx-auto text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-3xl text-red-600 font-bold">3000+</h3>
               </div>
               {/* icon section end */}
               {/* title & description section start */}
               <div>
-                <p className="text-center text-xs sm:text-lg md:text-xs lg:text-sm xl:text-md font-bold">Free Shipping</p>
-                <p className="text-center text-[10px] sm:text-sm md:text-[10px] lg:text-[11px] xl:text-xs text-white">Order Complete 99.99%</p>
+                <p className="text-center text-xs sm:text-sm md:text-sm lg:text-sm xl:text-base font-bold">Industry Expert</p>
+                <p className="text-center text-[10px] sm:text-xs md:text-sm lg:text-sm xl:text-sm text-white">Our Expert worker 99.99%</p>
               </div>
               {/* title & description section end */}
             </li>
           </ul>
           {/* item1 section end */}
-
+          {/* item2 section start */}
+          <ul>
+            <li>
+              {/* icon section start */}
+              <div>
+                <h3 className="text-center mx-auto text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-3xl text-red-600 font-bold">91%</h3>
+              </div>
+              {/* icon section end */}
+              {/* title & description section start */}
+              <div>
+                <p className="text-center text-xs sm:text-sm md:text-sm lg:text-sm xl:text-base font-bold">Success Ratio</p>
+                <p className="text-center text-[10px] sm:text-xs md:text-sm lg:text-sm xl:text-sm text-white">Our working rate 96.56%</p>
+              </div>
+              {/* title & description section end */}
+            </li>
+          </ul>
+          {/* item2 section end */}
+          {/* item3 section start */}
+          <ul>
+            <li>
+              {/* icon section start */}
+              <div>
+                <h3 className="text-center mx-auto text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-3xl text-red-600 font-bold">230+</h3>
+              </div>
+              {/* icon section end */}
+              {/* title & description section start */}
+              <div>
+                <p className="text-center text-xs sm:text-sm md:text-sm lg:text-sm xl:text-base font-bold">Companies</p>
+                <p className="text-center text-[10px] sm:text-xs md:text-sm lg:text-sm xl:text-sm text-white">Our export companies 80.56%</p>
+              </div>
+              {/* title & description section end */}
+            </li>
+          </ul>
+          {/* item3 section end */}
+          {/* item4 section start */}
+          <ul>
+            <li>
+              {/* icon section start */}
+              <div>
+                <h3 className="text-center mx-auto text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-3xl text-red-600 font-bold">200+</h3>
+              </div>
+              {/* icon section end */}
+              {/* title & description section start */}
+              <div>
+                <p className="text-center text-xs sm:text-sm md:text-sm lg:text-sm xl:text-base font-bold">Expert</p>
+                <p className="text-center text-[10px] sm:text-xs md:text-sm lg:text-sm xl:text-sm text-white">Expert worker 90.56%</p>
+              </div>
+              {/* title & description section end */}
+            </li>
+          </ul>
+          {/* item4 section end */}
+          {/* item5 section start */}
+          <ul>
+            <li>
+              {/* icon section start */}
+              <div>
+                <h3 className="text-center mx-auto text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-3xl text-red-600 font-bold">100%</h3>
+              </div>
+              {/* icon section end */}
+              {/* title & description section start */}
+              <div>
+                <p className="text-center text-xs sm:text-sm md:text-sm lg:text-sm xl:text-base font-bold">Successful</p>
+                <p className="text-center text-[10px] sm:text-xs md:text-sm lg:text-sm xl:text-sm text-white">Our Success 100%</p>
+              </div>
+              {/* title & description section end */}
+            </li>
+          </ul>
+          {/* item5 section end */}
         </div>
         {/* item section end */}
       </div>
