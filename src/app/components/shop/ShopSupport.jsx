e48afd1de681f81eed863224758bda16b4f8c37d
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-06-26 23:37:45 +0600

    creating shop support section

---
+++ src/app/components/shop/ShopSupport.jsx
@@ -0,0 +1,46 @@
+import { TiWorld } from "react-icons/ti";
+
+const ShopSupport = () => {
+  return (
+    <div className='my-10 sm:my-12 md:my-10 xl:my-16 px-5'>
+      {/* content section start */}
+      <div className='container mx-auto px-3 sm:px-5 md:px-3 lg:px-6 xl:px-8'>
+        {/* item section start */}
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-3 sm:gap-5 md:gap-2'>
+          {/* item1 section start */}
+          <div className="text-center mx-auto space-y-1">
+            <TiWorld className="mx-auto text-2xl sm:text-3xl md:text-2xl lg:text-3xl xl:text-4xl text-red-600"></TiWorld>
+            <h1 className="text-xs sm:text-sm md:text-xs lg:text-sm xl:text-lg font-bold">Worldwide Shipping</h1>
+            <p className="text-xs sm:text-sm md:text-[9px] lg:text-xs xl:text-base italic">Global delivery service with reliable logistics, ensuring timely and secure shipping worldwide</p>
+          </div>
+          {/* item1 section end */}
+          {/* item1 section start */}
+          <div className="text-center mx-auto space-y-1">
+            <TiWorld className="mx-auto text-2xl sm:text-3xl md:text-2xl lg:text-3xl xl:text-4xl text-red-600"></TiWorld>
+            <h1 className="text-xs sm:text-sm md:text-xs lg:text-sm xl:text-lg font-bold">Worldwide Shipping</h1>
+            <p className="text-xs sm:text-sm md:text-[9px] lg:text-xs xl:text-base italic">Global delivery service with reliable logistics, ensuring timely and secure shipping worldwide</p>
+          </div>
+          {/* item1 section end */}
+          {/* item1 section start */}
+          <div className="text-center mx-auto space-y-1">
+            <TiWorld className="mx-auto text-2xl sm:text-3xl md:text-2xl lg:text-3xl xl:text-4xl text-red-600"></TiWorld>
+            <h1 className="text-xs sm:text-sm md:text-xs lg:text-sm xl:text-lg font-bold">Worldwide Shipping</h1>
+            <p className="text-xs sm:text-sm md:text-[9px] lg:text-xs xl:text-base italic">Global delivery service with reliable logistics, ensuring timely and secure shipping worldwide</p>
+          </div>
+          {/* item1 section end */}
+          {/* item1 section start */}
+          <div className="text-center mx-auto space-y-1">
+            <TiWorld className="mx-auto text-2xl sm:text-3xl md:text-2xl lg:text-3xl xl:text-4xl text-red-600"></TiWorld>
+            <h1 className="text-xs sm:text-sm md:text-xs lg:text-sm xl:text-lg font-bold">Worldwide Shipping</h1>
+            <p className="text-xs sm:text-sm md:text-[9px] lg:text-xs xl:text-base italic">Global delivery service with reliable logistics, ensuring timely and secure shipping worldwide</p>
+          </div>
+          {/* item1 section end */}
+        </div>
+        {/* item section end */}
+      </div>
+      {/* content section end */}
+    </div>
+  );
+};
+
+export default ShopSupport;
--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -1,6 +1,7 @@
 import FeaturedProducts from '../components/shop/FeaturedProducts';
 import ShopBanner from '../components/shop/ShopBanner';
 import ShopBrand from '../components/shop/ShopBrand';
+import ShopSupport from '../components/shop/ShopSupport';
 import ViewProduct from '../components/shop/ViewProduct';
 
 const ShopPage = () => {
@@ -18,6 +19,9 @@ const ShopPage = () => {
       {/* featured products section start */}
       <FeaturedProducts></FeaturedProducts>
       {/* featured products section end */}
+      {/* shopSupport section start */}
+      <ShopSupport></ShopSupport>
+      {/* shopSupport section end */}
     </div>
   );
 };
