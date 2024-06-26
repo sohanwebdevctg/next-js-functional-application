cfd7d10f1cad11a902b0802c5ea75c024c8c34d8
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-06-26 14:48:59 +0600

    brnad logo section create

--- src/app/components/shop/ShopBanner.jsx
+++ src/app/components/shop/ShopBanner.jsx
@@ -1,4 +1,10 @@
-import React from 'react';
+import Image from 'next/image';
+import brand1 from '../../../../public/brandlogo/brand1.png';
+import brand2 from '../../../../public/brandlogo/brand4.png';
+import brand3 from '../../../../public/brandlogo/brand5.png';
+import brand4 from '../../../../public/brandlogo/brand6.png';
+import brand6 from '../../../../public/brandlogo/brand11.png';
+import brand7 from '../../../../public/brandlogo/brand12.png';
 
 const ShopBanner = () => {
   return (
@@ -14,6 +20,37 @@ const ShopBanner = () => {
         {/* item section end */}
       </div>
       {/* content section end */}
+      {/* logo section start */}
+      <div className='bg-[#F5F5F5] py-3'>
+        <ul className='container mx-auto grid grid-cols-8 gap-3'>
+          <li>
+            <Image src={brand1} alt="logo1" className='w-3/4 h-20'></Image>
+          </li>
+          <li>
+            <Image src={brand2} alt="logo2" className='w-3/4 h-20'></Image>
+          </li>
+          <li>
+            <Image src={brand4} alt="logo3" className='w-3/4 h-20'></Image>
+          </li>
+          <li>
+            <Image src={brand3} alt="logo6" className='w-3/4 h-20'></Image>
+          </li>
+          <li>
+            <Image src={brand6} alt="logo6" className='w-3/4 h-20'></Image>
+          </li>
+          <li>
+            <Image src={brand2} alt="logo6" className='w-3/4 h-20'></Image>
+          </li>
+          <li>
+            <Image src={brand7} alt="logo6" className='w-3/4 h-20'></Image>
+          </li>
+          <li>
+            <Image src={brand3} alt="logo6" className='w-3/4 h-20'></Image>
+          </li>
+        </ul>
+      </div>
+      
+      {/* logo section end */}
     </div>
   );
 };
