17166d783cfa6cf360e59230d2f468ac313110ba
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-07-06 00:48:08 +0600

    responsive the mobile and small device in single product

--- src/app/single/[id]/page.js
+++ src/app/single/[id]/page.js
@@ -49,17 +49,16 @@ const SingleProductPage = ({params,searchParams}) => {
 
   // quantity data
   const [quantity, setQuantity] = useState(1);
-
+  // quantity increment function
   const quantityIncrement = (data) => {
     setQuantity(data + 1)
   }
+  //quantity decrement function
   const quantityDecrement = (data) => {
     if(quantity > 1){
       setQuantity(data - 1)
     }
   }
-  console.log(quantity)
-
 
   // loading function checkbox
   if(!itemData){
@@ -68,19 +67,19 @@ const SingleProductPage = ({params,searchParams}) => {
 
   return (
     <div>
-          <div className='px-5 sm:px-4 md:px-4 lg:px-0 mt-5'>
+          <div className='px-3 sm:px-4 md:px-4 lg:px-0 my-10 sm:my-10'>
       {/* content section start */}
-      <div className='container mx-auto  h-full w-full py-10 px-3 sm:px-5 md:px-4 lg:px-10 xl:px-12 shadow-lg'>
+      <div className='container mx-auto h-full w-full py-5 px-2 sm:px-5 md:px-4 lg:px-10 xl:px-12 shadow-lg'>
         {/* item section start */}
         <div className="sm:flex sm:justify-between sm:gap-5 ">
           {/* image section start */}
-          <div className="w-full sm:w-[50%] p-10 bg-white">
+          <div className="w-full sm:w-[50%] p-3 sm:p-2 xl:p-10 bg-white">
             {/* selected image section start */}
             <div className="overflow-hidden">
-              <Image src={selectImage ? selectImage : (imageData ? imageData[0] : '/no-data')} width={300} height={300} alt="this is selected image" className='w-96 h-56 mx-auto'></Image>
-              <div className='flex  items-center justify-around gap-2 mt-5 overflow-hidden bg-[#F5F5F5] py-2'>
+              <Image src={selectImage ? selectImage : (imageData ? imageData[0] : '/no-data')} width={300} height={300} alt="this is selected image" className='w-full h-40 sm:w-full sm:h-44 xl:w-96 xl:h-56 mx-auto'></Image>
+              <div className='flex items-center justify-around gap-2 mt-5 overflow-x-auto bg-[#F5F5F5] py-2'>
                 {
-                  imageData?.map((data,index) => <Image key={index} src={data} width={100} height={100} alt={`this is ${data.id} image`} className='w-24 h-20' onClick={() => setSelectImage(data)}></Image>)
+                  imageData?.map((data,index) => <Image key={index} src={data} width={100} height={100} alt={`this is ${data.id} image`} className='w-16 h-16 sm:w-[70px] sm:h-[80px] xl:w-24 xl:h-20' onClick={() => setSelectImage(data)}></Image>)
                 }
               </div>
             </div>
@@ -92,42 +91,42 @@ const SingleProductPage = ({params,searchParams}) => {
           </div>
           {/* image section end */}
           {/* details section start */}
-          <div className="w-full sm:w-[50%] bg-[#F5F5F5] p-10">
-          <ul className='xl:space-y-3'>
+          <div className="w-full sm:w-[50%] bg-[#F5F5F5] p-3 xl:p-10 ">
+          <ul className='space-y-2 sm:space-y-4 xl:space-y-3'>
             {/* name and price */}
             <li className='flex items-center justify-between'>
-              <span className="xl:text-4xl font-bold">{itemData?.name}</span>
-              <span className="xl:text-3xl text-red-700 italic font-bold">$ {itemData?.price}</span>
+              <span className="text-lg sm:text-xl xl:text-4xl font-bold">{itemData?.name}</span>
+              <span className="text-lg sm:text-xl xl:text-3xl text-red-700 italic font-bold">$ {itemData?.price}</span>
             </li>
             {/* detsils */}
             <li>
-              <p className='xl:text-[15px] text-gray-500 xl:w-[80%]'>{itemData?.details}</p>
+              <p className='text-xs sm:text-[13px] xl:text-[15px] text-gray-500 w-full xl:w-[80%]'>{itemData?.details}</p>
             </li>
             {/* rating */}
             <li className="flex items-center">
-              <FaStar className="text-yellow-500 text-xs md:text-[9px] xl:text-lg"></FaStar>
-              <FaStar className="text-yellow-500 text-xs md:text-[9px] xl:text-lg"></FaStar>
-              <FaStar className="text-yellow-500 text-xs md:text-[9px] xl:text-lg"></FaStar>
-              <FaStar className="text-yellow-500 text-xs md:text-[9px] xl:text-lg"></FaStar>
-              <FaStar className="text-yellow-500 text-xs md:text-[9px] xl:text-lg"></FaStar>
-              <span className="text-xs md:text-[9px] xl:text-base ml-2"> {itemData?.rating}</span>
+              <FaStar className="text-yellow-500 text-xs md:text-[11px] xl:text-lg"></FaStar>
+              <FaStar className="text-yellow-500 text-xs md:text-[11px] xl:text-lg"></FaStar>
+              <FaStar className="text-yellow-500 text-xs md:text-[11px] xl:text-lg"></FaStar>
+              <FaStar className="text-yellow-500 text-xs md:text-[11px] xl:text-lg"></FaStar>
+              <FaStar className="text-yellow-500 text-xs md:text-[11px] xl:text-lg"></FaStar>
+              <span className="text-xs sm:text-sm md:text-[9px] xl:text-base ml-2"> {itemData?.rating}</span>
             </li>
             {/* color */}
-            <li className='flex items-center gap-6'>
-              <p className="xl:text-2xl">Color:</p>
+            <li className='flex items-center gap-6 sm:gap-3'>
+              <p className="text-sm sm:text-base xl:text-2xl">Color:</p>
               <p className='flex gap-1'>
               {
-                itemData?.colorItem?.map((data, index) => <span key={index} className={`text-${data.color}-500 text-3xl`} onClick={() => colorImage(data.image)}><MdInvertColors></MdInvertColors></span>)
+                itemData?.colorItem?.map((data, index) => <span key={index} className={`text-${data.color}-500 text-xl sm:text-xl xl:text-3xl`} onClick={() => colorImage(data.image)}><MdInvertColors></MdInvertColors></span>)
               }
               </p>
             </li>
             {/* quantity */}
             <li className='flex items-center gap-5'>
-              <p className="xl:text-2xl">Quantity:</p>
+              <p className="text-sm sm:text-base xl:text-2xl">Quantity:</p>
               <div className='flex justify-center items-center gap-2 '>
-              <FaPlusSquare onClick={() => quantityIncrement(quantity)} className="xl:text-2xl text-green-500"/>
-              <span className='xl:text-2xl'>{quantity}</span>
-              <FaMinusSquare onClick={() => quantityDecrement(quantity)} className="xl:text-2xl text-red-500"/>
+              <FaPlusSquare onClick={() => quantityIncrement(quantity)} className="text-sm sm:text-base xl:text-2xl text-green-500"/>
+              <span className='text-sm sm:text-base xl:text-2xl'>{quantity}</span>
+              <FaMinusSquare onClick={() => quantityDecrement(quantity)} className="text-sm sm:text-base xl:text-2xl text-red-500"/>
               </div>
             </li>
           </ul>
--- src/utilities/furniture.js
+++ src/utilities/furniture.js
@@ -8,7 +8,7 @@ export const furniture = [
       {
         id: 1,
         name: "Bed",
-        details: 'this is bed',
+        details: 'The plush mattress, cradled by a sturdy wooden frame, promises restful nights. Crisp white sheets beckon, inviting weary bodies to sink into serenity',
         price: 200,
         rating: 4.9,
         image : '/furnitures/bed/bed1/1/1.png',
@@ -55,7 +55,7 @@ export const furniture = [
       {
         id: 2,
         name: "Bed",
-        details: 'this is black bed',
+        details: 'A luxurious king-size bed, draped in cascading silk curtains, beckons for indulgence. The plush duvet and crisp white sheets promise a night of unparalleled comfort',
         price: 230,
         rating: 4.3,
         image : '/furnitures/bed/bed2/1/1.png',
@@ -102,7 +102,7 @@ export const furniture = [
       {
         id: 3,
         name: "Bed",
-        details: 'this is black bed',
+        details: "The sleek platform bed, a minimalist masterpiece, disappears beneath crisp white linens. Uncluttered and serene, it offers a haven of calm amidst the modern world's chaos",
         price: 100,
         rating: 4.2,
         image : '/furnitures/bed/bed3/1/1.png',
@@ -149,7 +149,7 @@ export const furniture = [
       {
         id: 4,
         name: "Bed",
-        details: 'this is coffe bed',
+        details: 'Weathered wood whispers stories of countless dreams. A fluffy down comforter, the color of sun-bleached wheat, promises warmth and comfort. Here, the world melts away, replaced by restful slumber',
         price: 260,
         rating: 3.9,
         image : '/furnitures/bed/bed4/1/1.png',
@@ -196,7 +196,7 @@ export const furniture = [
       {
         id: 5,
         name: "Bed",
-        details: 'this is my bed',
+        details: 'A canopy bed, transformed into a magical haven, shimmers with cascading fairy lights. Soft, billowing curtains frame a dreamland where imagination takes flight and sweet dreams take hold',
         price: 100,
         rating: 4.6,
         image : '/furnitures/bed/bed5/1/1.png',
@@ -251,7 +251,7 @@ export const furniture = [
       {
         id: 1,
         name: "Chair",
-        details: 'this is Chair',
+        details: 'Material (Mesh), Weight Capacity (300 lbs), Adjustable Height (Yes), Lumbar Support (Yes), Armrests (Padded, Adjustable)',
         price: 200,
         rating: 4.9,
         image : '/furnitures/chair/chair1/1/1.png',
@@ -288,7 +288,7 @@ export const furniture = [
       {
         id: 2,
         name: "Chair",
-        details: 'this is Chair',
+        details: 'Style (Mid-Century Modern), Material (Upholstered), Color (Navy Blue), Leg Material (Metal), Stackable (Yes)',
         price: 230,
         rating: 4.3,
         image : '/furnitures/chair/chair2/1/1.png',
@@ -325,7 +325,7 @@ export const furniture = [
       {
         id: 3,
         name: "Chair",
-        details: 'this is Chair',
+        details: ' Design (Racing-inspired), Reclining (Up to 135 degrees), Material (PU Leather), Headrest and Lumbar Support (Included), Adjustable Armrests (3D)',
         price: 100,
         rating: 4.2,
         image : '/furnitures/chair/chair3/1/1.png',
@@ -362,7 +362,7 @@ export const furniture = [
       {
         id: 4,
         name: "Chair",
-        details: 'this is Chair',
+        details: 'Style (Classic), Material (Genuine Leather), Color (Tan), Deep Seat (Yes), Nailhead Trim (Yes)',
         price: 260,
         rating: 3.9,
         image : '/furnitures/chair/chair4/1/1.png',
@@ -399,7 +399,7 @@ export const furniture = [
       {
         id: 5,
         name: "Chair",
-        details: 'this is Chair',
+        details: ' Material (Canvas), Frame (Aluminum), Weight Capacity (250 lbs), Portable (Folds Flat), Carrying Case (Included)',
         price: 100,
         rating: 4.6,
         image : '/furnitures/chair/chair5/1/1.png',
@@ -444,7 +444,7 @@ export const furniture = [
       {
         id: 1,
         name: "DoubleSofa",
-        details: 'this is doublesofa',
+        details: 'Featuring rolled arms and plush cushions, this timeless design offers a comfortable and inviting seating option for two',
         price: 200,
         rating: 4.9,
         image : '/furnitures/doubleSofa/doubleSofa1/1/1.png',
@@ -491,7 +491,7 @@ export const furniture = [
       {
         id: 2,
         name: "DoubleSofa",
-        details: 'this is doublesofa',
+        details: 'With clean lines and a sleek silhouette, this contemporary loveseat provides a touch of style to any living space. Perfect for smaller apartments or minimalist living',
         price: 230,
         rating: 4.3,
         image : '/furnitures/doubleSofa/doubleSofa2/1/1.png',
@@ -538,7 +538,7 @@ export const furniture = [
       {
         id: 3,
         name: "DoubleSofa",
-        details: 'this is doublesofa',
+        details: 'This versatile loveseat transforms into a comfy bed for unexpected guests, making it ideal for maximizing space in studios or guest rooms',
         price: 100,
         rating: 4.2,
         image : '/furnitures/doubleSofa/doubleSofa3/1/1.png',
@@ -585,7 +585,7 @@ export const furniture = [
       {
         id: 4,
         name: "DoubleSofa",
-        details: 'this is doublesofa',
+        details: ' Indulge in ultimate relaxation with this luxurious option. The chaise lounge provides ample space to stretch out, while the loveseat offers comfortable seating for two',
         price: 260,
         rating: 3.9,
         image : '/furnitures/doubleSofa/doubleSofa4/1/1.png',
@@ -632,7 +632,7 @@ export const furniture = [
       {
         id: 5,
         name: "DoubleSofa",
-        details: 'this is doublesofa',
+        details: 'Kick back and relax with this loveseat featuring reclining seats. Perfect for movie nights or enjoying a good book in ultimate comfort',
         price: 100,
         rating: 4.6,
         image : '/furnitures/doubleSofa/doubleSofa5/1/1.png',
@@ -687,7 +687,7 @@ export const furniture = [
       {
         id: 1,
         name: "SingleSofa",
-        details: 'this is single sofa',
+        details: 'Plush velvet envelops a sturdy frame, offering timeless elegance. Deep seat invites curling up with a good book',
         price: 200,
         rating: 4.9,
         image : '/furnitures/singleSofa/singleSofa1/1/1.png',
@@ -734,7 +734,7 @@ export const furniture = [
       {
         id: 2,
         name: "SingleSofa",
-        details: 'this is single sofa',
+        details: 'Crisp white linen hugs a sleek, low-profile frame. Perfect for minimalist living, it adds a touch of clean-lined sophistication',
         price: 230,
         rating: 4.3,
         image : '/furnitures/singleSofa/singleSofa2/1/1.png',
@@ -781,7 +781,7 @@ export const furniture = [
       {
         id: 3,
         name: "SingleSofa",
-        details: 'this is single sofa',
+        details: 'Weathered leather whispers tales of comfort. A plush throw blanket adds a warm touch, inviting relaxation in a haven of serenity',
         price: 100,
         rating: 4.2,
         image : '/furnitures/singleSofa/singleSofa3/1/1.png',
@@ -828,7 +828,7 @@ export const furniture = [
       {
         id: 4,
         name: "SingleSofa",
-        details: 'this is single sofa',
+        details: ' A bold statement piece, this velvet armchair features a vibrant jewel tone. Its unique design adds a pop of color and personality to any space',
         price: 260,
         rating: 3.9,
         image : '/furnitures/singleSofa/singleSofa4/1/1.png',
@@ -875,7 +875,7 @@ export const furniture = [
       {
         id: 5,
         name: "SingleSofa",
-        details: 'this is single sofa',
+        details: 'This sleek chair effortlessly swivels, offering a touch of playful functionality. Perfect for a reading nook or conversation starter, it invites comfortable lounging',
         price: 100,
         rating: 4.6,
         image : '/furnitures/singleSofa/singleSofa5/1/1.png',
@@ -930,7 +930,7 @@ export const furniture = [
       {
         id: 1,
         name: "SofaSet",
-        details: 'this is sofa set',
+        details: 'Sink into the pillowy comfort of this plush sofa set. Deep cushions and a generous size invite movie nights with loved ones',
         price: 200,
         rating: 4.9,
         image : '/furnitures/sofaSet/sofaSet1/1/1.png',
@@ -977,7 +977,7 @@ export const furniture = [
       {
         id: 2,
         name: "SofaSet",
-        details: 'this is sofa set',
+        details: 'Clean lines and sleek upholstery define this contemporary sofa set. Perfect for open floor plans, it creates a minimalist haven',
         price: 230,
         rating: 4.3,
         image : '/furnitures/sofaSet/sofaSet2/1/1.png',
@@ -1024,7 +1024,7 @@ export const furniture = [
       {
         id: 3,
         name: "SofaSet",
-        details: 'this is sofa set',
+        details: 'This inviting sofa set, wrapped in warm chenille, beckons for relaxation. Plush cushions and a matching loveseat create a cozy haven for family gatherings',
         price: 100,
         rating: 4.2,
         image : '/furnitures/sofaSet/sofaSet3/1/1.png',
@@ -1071,7 +1071,7 @@ export const furniture = [
       {
         id: 4,
         name: "SofaSet",
-        details: 'this is sofa set',
+        details: 'Indulge in opulence with this luxurious sofa set. Rich leather and intricate detailing create a sophisticated statement piece in any living room',
         price: 260,
         rating: 3.9,
         image : '/furnitures/sofaSet/sofaSet4/1/1.png',
@@ -1118,7 +1118,7 @@ export const furniture = [
       {
         id: 5,
         name: "SofaSet",
-        details: 'this is sofa set',
+        details: 'This versatile sectional sofa set offers endless configurations. Relax on the spacious chaise, or create a cozy nook for reading – it adapts to your needs',
         price: 100,
         rating: 4.6,
         image : '/furnitures/sofaSet/sofaSet5/1/1.png',
@@ -1173,7 +1173,7 @@ export const furniture = [
       {
         id: 1,
         name: "Table",
-        details: 'this is Table',
+        details: 'A sturdy wooden table with a timeless design offers a warm and inviting space for meals and gatherings. Tapered legs and a smooth finish exude elegance',
         price: 200,
         rating: 4.9,
         image : '/furnitures/table/table1/1/1.png',
@@ -1220,7 +1220,7 @@ export const furniture = [
       {
         id: 2,
         name: "Table",
-        details: 'this is Table',
+        details: 'This sleek table, crafted from polished glass and chrome, creates a minimalist statement. The clean lines and reflective surface add a touch of contemporary sophistication',
         price: 230,
         rating: 4.3,
         image : '/furnitures/table/table2/1/1.png',
@@ -1267,7 +1267,7 @@ export const furniture = [
       {
         id: 3,
         name: "Table",
-        details: 'this is Table',
+        details: 'A reclaimed wood table, boasting a weathered finish, tells stories of time and craftsmanship. Perfect for farmhouse-inspired spaces, it creates a warm and inviting atmosphere',
         price: 100,
         rating: 4.2,
         image : '/furnitures/table/table3/1/1.png',
@@ -1314,7 +1314,7 @@ export const furniture = [
       {
         id: 4,
         name: "Table",
-        details: 'this is Table',
+        details: 'This versatile table extends to accommodate large gatherings. Its hidden leaf mechanism ensures a seamless transition from intimate dinners to spacious celebrations',
         price: 260,
         rating: 3.9,
         image : '/furnitures/table/table4/1/1.png',
@@ -1361,7 +1361,7 @@ export const furniture = [
       {
         id: 5,
         name: "Table",
-        details: 'this is Table',
+        details: 'More than just a table, this piece transforms into a coffee table or dining surface. Ideal for studios or small apartments, it maximizes space without sacrificing functionality',
         price: 100,
         rating: 4.6,
         image : '/furnitures/table/table5/1/1.png',
@@ -1416,7 +1416,7 @@ export const furniture = [
       {
         id: 1,
         name: "TeaTable",
-        details: 'this is tea table',
+        details: 'A timeless round table with a warm wood finish invites cozy tea parties. Delicate legs and a smooth surface create a feeling of elegance for intimate gatherings',
         price: 200,
         rating: 4.9,
         image : '/furnitures/teaTable/teaTable1/1/1.png',
@@ -1463,7 +1463,7 @@ export const furniture = [
       {
         id: 2,
         name: "TeaTable",
-        details: 'this is tea table',
+        details: 'This sleek nesting table set features two tables in contrasting sizes. Made of polished glass and chrome, it offers a minimalist touch with a playful nesting functionality',
         price: 230,
         rating: 4.3,
         image : '/furnitures/teaTable/teaTable2/1/1.png',
@@ -1510,7 +1510,7 @@ export const furniture = [
       {
         id: 3,
         name: "TeaTable",
-        details: 'this is tea table',
+        details: ' A reclaimed wood tea table, boasting a natural wood grain and a weathered finish,  adds warmth to any space. Perfect for displaying decorative accents or enjoying a cup of tea in a cozy setting',
         price: 100,
         rating: 4.2,
         image : '/furnitures/teaTable/teaTable3/1/1.png',
@@ -1557,7 +1557,7 @@ export const furniture = [
       {
         id: 4,
         name: "TeaTable",
-        details: 'this is tea table',
+        details: 'This ingenious tea table features a hidden storage compartment ideal for stowing away magazines, blankets, or tea supplies. Its sleek design seamlessly integrates with your living space',
         price: 260,
         rating: 3.9,
         image : '/furnitures/teaTable/teaTable4/1/1.png',
@@ -1604,7 +1604,7 @@ export const furniture = [
       {
         id: 5,
         name: "TeaTable",
-        details: 'this is tea table',
+        details: 'More than just a tea table, this multifunctional piece features a lift-top mechanism that transforms it into a convenient workspace. Perfect for small apartments, it maximizes space and functionality',
         price: 100,
         rating: 4.6,
         image : '/furnitures/teaTable/teaTable5/1/1.png',
