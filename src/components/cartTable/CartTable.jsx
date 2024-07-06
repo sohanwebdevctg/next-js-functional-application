43d4f9562f15bed5ba418764755b58419f659ed7
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-07-07 01:12:25 +0600

    creating data table

--- src/components/cartTable/CartTable.jsx
+++ src/components/cartTable/CartTable.jsx
@@ -1,51 +1,90 @@
-import React from "react";
+import { MdInvertColors } from "react-icons/md";
+
+const tableData = [
+  {
+    id: 1,
+    name: 'chair',
+    quantity: 3,
+    price: 30,
+    color: 'black'
+  },
+  {
+    id: 2,
+    name: 'chair',
+    quantity: 3,
+    price: 30,
+    color: 'black'
+  },
+  {
+    id: 3,
+    name: 'chair',
+    quantity: 3,
+    price: 30,
+    color: 'black'
+  },
+  {
+    id: 4,
+    name: 'chair',
+    quantity: 3,
+    price: 30,
+    color: 'black'
+  }
+]
 
 const CartTable = () => {
+
+
+
   return (
     <div className="my-10 px-4">
       {/* content section start */}
       <div className="container bg-[#F5F5F5] h-full w-full mx-auto py-10 md:py-8 lg:py-14 xl:py-16 px-3 sm:px-5 md:px-2 lg:px-10 xl:px-12">
+        <div className="overflow-x-auto space-y-5">
+        {/* search section start */}
+        <div className="bg-white w-2/3 p-2 mx-auto">
+          <ul className="flex w-full justify-center gap-2">
+            <li><input type="text" placeholder="search product name" className="input input-bordered input-sm w-full" /></li>
+            <li><input type="text" placeholder="search quantity" className="input input-bordered input-sm w-full" /></li>
+            <li><input type="text" placeholder="search price" className="input input-bordered input-sm w-full" /></li>
+            <li className="w-"><select className="select select-bordered select-sm w-full">
+              <option disabled selected>Price</option>
+              <option>High to Low</option>
+              <option>Low to High</option>
+            </select>
+            </li>
+          </ul>
+        </div>
+        {/* search section end */}
         {/* table section start */}
-        <div className="overflow-x-auto">
           <table className="table">
             {/* head */}
             <thead className="bg-red-500 text-white">
-              <tr>
+              <tr className="text-center">
                 <th>#</th>
+                <th>Image</th>
                 <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
-                <th>Details</th>
+                <th>Quantity</th>
+                <th>Color</th>
+                <th>Price</th>
+                <th>Delete</th>
               </tr>
             </thead>
             <tbody>
               {/* row 1 */}
-              <tr className="hover:bg-red-100">
+              {
+                tableData.map((data, index)=><tr key={data.id} className="hover:bg-red-100 text-center">
+                <td className="xl:text-sm">{++index}</td>
+                <td className="xl:text-sm">Image</td>
+                <td className="xl:text-sm">{data.name}</td>
+                <td className="xl:text-sm">{data.quantity}</td>
+                <td className="xl:text-xl"><MdInvertColors className={`text-${data.color} text-center mx-auto`}></MdInvertColors></td>
+                <td>$ {data.price}</td>
                 <th>
-                  <label>
-                    <input type="checkbox" className="checkbox" />
-                  </label>
+                  <button className="btn btn-xs bg-red-500 text-white hover:bg-red-500">Delete</button>
                 </th>
-                <td>
-                  <div>
-                    <div className="avatar">
-                      <div className="mask mask-squircle h-12 w-12">
-                        <img
-                          src="https://img.daisyui.com/tailwind-css-component-profile-2@56w.png"
-                          alt="Avatar Tailwind CSS Component"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  Zemlak, Daniel and Leannon
-                </td>
-                <td>Purple</td>
-                <th>
-                  <button className="btn btn-ghost btn-xs">details</button>
-                </th>
-              </tr>
+              </tr>)
+              }
+
             </tbody>
           </table>
         </div>
