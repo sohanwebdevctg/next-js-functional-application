9cb5df4c52ace117e888c11de43cf59e76710c57
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-08-16 22:14:07 +0600

    adding data localstorage from cart data

--- src/app/single/[id]/page.js
+++ src/app/single/[id]/page.js
@@ -1,171 +1,266 @@
-'use client'
+"use client";
 
-import { FaStar } from 'react-icons/fa';
+import { FaStar } from "react-icons/fa";
 import { MdInvertColors } from "react-icons/md";
-import Image from 'next/image';
-import { furniture } from '@/utilities/furniture';
-import { useEffect, useState } from 'react';
+import Image from "next/image";
+import { furniture } from "@/utilities/furniture";
+import { useEffect, useState } from "react";
 import { FaPlusSquare, FaMinusSquare } from "react-icons/fa";
-import { useRouter } from 'next/navigation';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useRouter } from "next/navigation";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-
-const SingleProductPage = ({params,searchParams}) => {
-
+const SingleProductPage = ({ params, searchParams }) => {
   // params and searchParams
-  const {id} = params;
-  const {name} = searchParams;
-  const route = useRouter()
+  const { id } = params;
+  const { name } = searchParams;
+  const route = useRouter();
 
-    // furniture fetching data
-    const furnitureData = furniture;
+  // furniture fetching data
+  const furnitureData = furniture;
 
   // find out data from params and set state
   useEffect(() => {
+    if (id && name) {
+      const matchItem = furnitureData.find((category) =>
+        category.items.some(
+          (item) => item.id === parseInt(id) && item.name === name
+        )
+      );
 
-    if(id && name){
-      const matchItem = furnitureData.find((category) => category.items.some((item) => item.id === parseInt(id) && item.name === name))
-
-      if(matchItem){
-        const item = matchItem.items.find((item) => item.id === parseInt(id) && item.name === name);
-        setItemData(item)
-        setImageData(item?.colorItem[0].image)
+      if (matchItem) {
+        const item = matchItem.items.find(
+          (item) => item.id === parseInt(id) && item.name === name
+        );
+        setItemData(item);
+        setImageData(item?.colorItem[0].image);
       }
-    };
+    }
+  }, [id, name]);
 
-  },[id,name])
-
-   // set find data
+  // set find data
   const [itemData, setItemData] = useState([]);
 
-
-   // setImageData from color plate
+  // setImageData from color plate
   const [imageData, setImageData] = useState(null);
 
   //  set color image function
   const colorImage = (data) => {
     setImageData(data);
-    setSelectImage(data[0])
-  }
+    setSelectImage(data[0]);
+  };
   // set selected data from onClick function
-  const [selectImage, setSelectImage] = useState('')
+  const [selectImage, setSelectImage] = useState("");
+
+  // color setting
+  const [color, setColor] = useState(null);
+
+  // const colorFun = (data) => {
+  //   if(color == null){
+  //     alert('please select a color');
+  //   }else{
+  //     setColor(data)
+  //   }
+  // }
 
   // quantity data
   const [quantity, setQuantity] = useState(1);
 
   // quantity increment function
   const quantityIncrement = (data) => {
-    setQuantity(data + 1)
-  }
+    setQuantity(data + 1);
+  };
 
   //quantity decrement function
   const quantityDecrement = (data) => {
-    if(quantity > 1){
-      setQuantity(data - 1)
+    if (quantity > 1) {
+      setQuantity(data - 1);
     }
-  }
+  };
 
   //add data in localStorage
   const addData = () => {
-    toast("Your data added")
-  }
+
+    //get all data from cart
+    const cartItem = {
+      image: imageData,
+      color: color === null ? itemData?.colorItem[0]?.color : color,
+      name: itemData.name,
+      quantity: quantity,
+      price: itemData?.price,
+      total: quantity * itemData?.price,
+    };
+
+    //get previous cartData
+    const cartData = localStorage.getItem('cartData');
+
+    //check cartData
+    const cartArray = cartData ? JSON.parse(cartData) : [];
+
+    //adding new cartItem in cartArray
+    cartArray.push(cartItem)
+
+    //set the update array data localStorage
+    localStorage.setItem("cartData", JSON.stringify(cartArray));
+
+    if (cartArray.length > 0) {
+      route.push("/cart");
+      toast("Your data added");
+    }
+
+  };
 
   // previous page
   const previousPage = () => {
-    route.push('/')
-  }
-
+    route.push("/");
+  };
 
   // loading function checkbox
-  if(!itemData){
-    return <p>Loading.....</p>
+  if (!itemData) {
+    return <p>Loading.....</p>;
   }
 
   return (
     <div>
-          <div className='px-3 sm:px-4 md:px-4 lg:px-6 my-10 sm:my-10'>
-      {/* content section start */}
-      <div className='container mx-auto h-full w-full py-5 px-2 sm:px-5 md:px-4 lg:px-8 xl:px-12 shadow-lg'>
-        {/* item section start */}
-        <div className="sm:flex sm:justify-between sm:gap-5 ">
-          {/* image section start */}
-          <div className="w-full sm:w-[50%] p-3 sm:p-2 xl:p-10 bg-white">
-            {/* selected image section start */}
-            <div className="overflow-hidden">
-              <Image src={selectImage ? selectImage : (imageData ? imageData[0] : '/no-data')} width={300} height={300} alt="this is selected image" className='w-full h-40 sm:w-full sm:h-44 md:w-72 md:h-48 lg:w-80 lg:h-52 xl:w-96 xl:h-60 mx-auto'></Image>
-              <div className='flex items-center justify-around gap-2 mt-5 overflow-x-auto bg-[#F5F5F5] py-2'>
-                {
-                  imageData?.map((data,index) => <Image key={index} src={data} width={100} height={100} alt={`this is ${data.id} image`} className='w-16 h-16 sm:w-[70px] sm:h-[80px] md:w-[80px] md:h-[86px] lg:w-20 lg:h-20 xl:w-20 xl:h-24' onClick={() => setSelectImage(data)}></Image>)
-                }
+      <div className="px-3 sm:px-4 md:px-4 lg:px-6 my-10 sm:my-10">
+        {/* content section start */}
+        <div className="container mx-auto h-full w-full py-5 px-2 sm:px-5 md:px-4 lg:px-8 xl:px-12 shadow-lg">
+          {/* item section start */}
+          <div className="sm:flex sm:justify-between sm:gap-5 ">
+            {/* image section start */}
+            <div className="w-full sm:w-[50%] p-3 sm:p-2 xl:p-10 bg-white">
+              {/* selected image section start */}
+              <div className="overflow-hidden">
+                <Image
+                  src={
+                    selectImage
+                      ? selectImage
+                      : imageData
+                      ? imageData[0]
+                      : "/no-data"
+                  }
+                  width={300}
+                  height={300}
+                  alt="this is selected image"
+                  className="w-full h-40 sm:w-full sm:h-44 md:w-72 md:h-48 lg:w-80 lg:h-52 xl:w-96 xl:h-60 mx-auto"
+                ></Image>
+                <div className="flex items-center justify-around gap-2 mt-5 overflow-x-auto bg-[#F5F5F5] py-2">
+                  {imageData?.map((data, index) => (
+                    <Image
+                      key={index}
+                      src={data}
+                      width={100}
+                      height={100}
+                      alt={`this is ${data.id} image`}
+                      className="w-16 h-16 sm:w-[70px] sm:h-[80px] md:w-[80px] md:h-[86px] lg:w-20 lg:h-20 xl:w-20 xl:h-24"
+                      onClick={() => setSelectImage(data)}
+                    ></Image>
+                  ))}
+                </div>
               </div>
+              {/* selected image section end */}
+              {/* select image section start */}
+              <div></div>
+              {/* select image section end */}
             </div>
-            {/* selected image section end */}
-            {/* select image section start */}
-            <div>
+            {/* image section end */}
+            {/* details section start */}
+            <div className="w-full sm:w-[50%] bg-[#F5F5F5] p-3 lg:p-5 xl:p-10 ">
+              <ul className="space-y-2 sm:space-y-4 md:space-y-3 lg:space-y-3 xl:space-y-4">
+                {/* name and price */}
+                <li className="flex items-center justify-between">
+                  <span className="text-lg sm:text-xl md:text-xl lg:text-2xl xl:text-4xl font-bold">
+                    {itemData?.name}
+                  </span>
+                  <span className="text-lg sm:text-xl md:text-xl lg:text-2xl xl:text-3xl text-red-700 italic font-bold">
+                    $ {itemData?.price}
+                  </span>
+                </li>
+                {/* details */}
+                <li>
+                  <p className="text-xs sm:text-[13px] md:text-sm lg:text-[15px] xl:text-base text-gray-500 w-full xl:w-[90%]">
+                    {itemData?.details}
+                  </p>
+                </li>
+                {/* rating */}
+                <li className="flex items-center">
+                  <FaStar className="text-yellow-500 text-xs md:text-sm lg:text-base xl:text-lg"></FaStar>
+                  <FaStar className="text-yellow-500 text-xs md:text-sm lg:text-base xl:text-lg"></FaStar>
+                  <FaStar className="text-yellow-500 text-xs md:text-sm lg:text-base xl:text-lg"></FaStar>
+                  <FaStar className="text-yellow-500 text-xs md:text-sm lg:text-base xl:text-lg"></FaStar>
+                  <FaStar className="text-yellow-500 text-xs md:text-sm lg:text-base xl:text-lg"></FaStar>
+                  <span className="text-xs sm:text-sm md:text-sm lg:text-base xl:text-lg ml-2">
+                    {" "}
+                    {itemData?.rating}
+                  </span>
+                </li>
+                {/* color */}
+                <li className="flex items-center gap-6 sm:gap-3 md:gap-3 lg:gap-4 xl:gp-6">
+                  <p className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+                    Color:
+                  </p>
+                  <p className="flex gap-1">
+                    {itemData?.colorItem?.map((data, index) => (
+                      <span
+                        key={index}
+                        style={{ color: data.color }}
+                        className={` text-xl sm:text-xl md:text-2xl lg:text-2xl xl:text-3xl`}
+                        onClick={() => setColor(data.color)}
+                      >
+                        <MdInvertColors
+                          onClick={() => colorImage(data.image)}
+                        ></MdInvertColors>
+                      </span>
+                    ))}
+                  </p>
+                </li>
+                {/* quantity */}
+                <li className="flex items-center gap-5">
+                  <p className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+                    Quantity:
+                  </p>
+                  <div className="flex justify-center items-center gap-2 ">
+                    <FaPlusSquare
+                      onClick={() => quantityIncrement(quantity)}
+                      className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl text-green-500"
+                    />
+                    <span className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+                      {quantity}
+                    </span>
+                    <FaMinusSquare
+                      onClick={() => quantityDecrement(quantity)}
+                      className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl text-red-500"
+                    />
+                  </div>
+                </li>
+                {/* total price */}
+                <p className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+                  Total: $ {itemData?.price * quantity}
+                </p>
+                {/* button  */}
+                <li>
+                  <button
+                    onClick={addData}
+                    className="bg-green-600 text-white btn btn-xs sm:btn-xs md:btn-sm lg:btn-sm xl:btn-md hover:bg-green-600"
+                  >
+                    Add to Cart
+                  </button>
+                  <button
+                    onClick={previousPage}
+                    className="bg-red-600 text-white btn btn-xs sm:btn-xs md:btn-sm lg:btn-sm xl:btn-md hover:bg-red-600 ml-2"
+                  >
+                    Back
+                  </button>
+                </li>
+              </ul>
             </div>
-            {/* select image section end */}
+            {/* details section end */}
           </div>
-          {/* image section end */}
-          {/* details section start */}
-          <div className="w-full sm:w-[50%] bg-[#F5F5F5] p-3 lg:p-5 xl:p-10 ">
-          <ul className='space-y-2 sm:space-y-4 md:space-y-3 lg:space-y-3 xl:space-y-4'>
-            {/* name and price */}
-            <li className='flex items-center justify-between'>
-              <span className="text-lg sm:text-xl md:text-xl lg:text-2xl xl:text-4xl font-bold">{itemData?.name}</span>
-              <span className="text-lg sm:text-xl md:text-xl lg:text-2xl xl:text-3xl text-red-700 italic font-bold">$ {itemData?.price}</span>
-            </li>
-            {/* details */}
-            <li>
-              <p className='text-xs sm:text-[13px] md:text-sm lg:text-[15px] xl:text-base text-gray-500 w-full xl:w-[90%]'>{itemData?.details}</p>
-            </li>
-            {/* rating */}
-            <li className="flex items-center">
-              <FaStar className="text-yellow-500 text-xs md:text-sm lg:text-base xl:text-lg"></FaStar>
-              <FaStar className="text-yellow-500 text-xs md:text-sm lg:text-base xl:text-lg"></FaStar>
-              <FaStar className="text-yellow-500 text-xs md:text-sm lg:text-base xl:text-lg"></FaStar>
-              <FaStar className="text-yellow-500 text-xs md:text-sm lg:text-base xl:text-lg"></FaStar>
-              <FaStar className="text-yellow-500 text-xs md:text-sm lg:text-base xl:text-lg"></FaStar>
-              <span className="text-xs sm:text-sm md:text-sm lg:text-base xl:text-lg ml-2"> {itemData?.rating}</span>
-            </li>
-            {/* color */}
-            <li className='flex items-center gap-6 sm:gap-3 md:gap-3 lg:gap-4 xl:gp-6'>
-              <p className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">Color:</p>
-              <p className='flex gap-1'>
-              {/* {
-                itemData?.colorItem?.map((data, index) => <span key={index} className={`text-[${data?.color}] text-xl sm:text-xl md:text-2xl lg:text-2xl xl:text-3xl`} onClick={() => colorImage(data.image)}><MdInvertColors></MdInvertColors></span>)
-              } */}
-              {
-                itemData?.colorItem?.map((data, index) => <span key={index} 
-                style={{ color: data.color }} className={` text-xl sm:text-xl md:text-2xl lg:text-2xl xl:text-3xl`} onClick={() => colorImage(data.image)}><MdInvertColors></MdInvertColors></span>)
-              }
-              </p>
-            </li>
-            {/* quantity */}
-            <li className='flex items-center gap-5'>
-              <p className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">Quantity:</p>
-              <div className='flex justify-center items-center gap-2 '>
-              <FaPlusSquare onClick={() => quantityIncrement(quantity)} className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl text-green-500"/>
-              <span className='text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl'>{quantity}</span>
-              <FaMinusSquare onClick={() => quantityDecrement(quantity)} className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl text-red-500"/>
-              </div>
-            </li>
-            {/* total price */}
-            <p className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">Total: $ {itemData?.price * quantity}</p>
-            {/* button  */}
-            <li>
-              <button onClick={addData} className='bg-green-600 text-white btn btn-xs sm:btn-xs md:btn-sm lg:btn-sm xl:btn-md hover:bg-green-600'>Add to Cart</button>
-              <button onClick={previousPage} className='bg-red-600 text-white btn btn-xs sm:btn-xs md:btn-sm lg:btn-sm xl:btn-md hover:bg-red-600 ml-2'>Back</button>
-            </li>
-          </ul>
-          </div>
-          {/* details section end */}
+          {/* item section end */}
         </div>
-        {/* item section end */}
+        {/* content section end */}
+        <ToastContainer />
       </div>
-      {/* content section end */}
-      <ToastContainer />
-    </div>
     </div>
   );
 };
--- src/components/cartTable/CartTable.jsx
+++ src/components/cartTable/CartTable.jsx
@@ -116,7 +116,7 @@ const CartTable = () => {
       <div className="container bg-[#F5F5F5] h-full w-full mx-auto py-10 md:py-8 lg:py-14 xl:py-16 px-3 sm:px-5 md:px-2 lg:px-10 xl:px-12">
         <div className="overflow-x-auto space-y-5">
           {/* search section start */}
-          <div className="bg-white w-96 md:w-11/12 xl:w-5/6 p-2 mx-auto ml-10 sm:ml-20 md:ml-8 lg:ml-8 xl:ml-16 2xl:ml-20">
+          <div className="bg-white w-96 md:w-11/12 xl:w-5/6 p-2 mx-auto ml-10 sm:ml-20 md:ml-8 lg:ml-8 xl:ml-20 2xl:ml-24">
             <ul className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 w-full gap-2 sm:gap-3 md:gap-2 lg:gap-3 xl:gap-3 2xl:gap-4">
               <li className="w-full">
                 <input
@@ -165,9 +165,10 @@ const CartTable = () => {
                 <th>#</th>
                 <th>Image</th>
                 <th>Name</th>
-                <th>Quantity</th>
                 <th>Color</th>
+                <th>Quantity</th>
                 <th>Price</th>
+                <th>Total</th>
                 <th>Delete</th>
               </tr>
             </thead>
@@ -179,13 +180,14 @@ const CartTable = () => {
                     <td className="xl:text-sm">{++index}</td>
                     <td className="xl:text-sm">Image</td>
                     <td className="xl:text-sm">{data.name}</td>
-                    <td className="xl:text-sm">{data.quantity}</td>
                     <td className="xl:text-xl">
                       <MdInvertColors
                         className={`text-${data.color} text-center mx-auto`}
                       ></MdInvertColors>
                     </td>
+                    <td className="xl:text-sm">{data.quantity}</td>
                     <td>$ {data.price}</td>
+                    <td>$ {"total"}</td>
                     <th>
                       <button className="btn btn-xs bg-red-500 text-white hover:bg-red-500">
                         Delete
